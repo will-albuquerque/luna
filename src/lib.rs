6aad05f32fde9b038981cfe1c3d5449f09184b22
Will Albuquerque <w.j.albuquerque@outlook.com>
2024-05-03 21:55:48 +0100

    Add RandomPoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+use imageproc::point::Point;
+use rand::{
+    distributions::{Distribution, Uniform},
+    thread_rng,
+};
+
+use crate::random_point::RandomPoint;
+
+mod random_point;
+
 pub fn run() {
-    println!("Hello, world!");
+    let mut rng = thread_rng();
+    let uniform = Uniform::new(
+        RandomPoint::new(Point { x: 0, y: 0 }),
+        RandomPoint::new(Point { x: 1, y: 2 }),
+    );
+    println!("{:?}", uniform.sample(&mut rng).into_inner());
 }
