ec6b8a283ba09f92eb83828f9fabb0a1068c7021
Will Albuquerque <w.j.albuquerque@outlook.com>
2024-05-06 17:01:35 +0100

    Simplify into lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,40 @@
-use bivariate::Bivariate;
+use imageproc::point::Point;
 use rand::{
     distributions::{Distribution, Uniform},
-    thread_rng,
+    thread_rng, Rng,
 };
 
-mod bivariate;
-mod triangle;
+#[derive(Debug)]
+struct Polygon<T, const N: usize> {
+    vertices: [Point<T>; N],
+}
+
+struct Bivariate<X, Y> {
+    x: X,
+    y: Y,
+}
+
+impl<T, const N: usize, X, Y> Distribution<Polygon<T, N>> for Bivariate<X, Y>
+where
+    X: Distribution<T>,
+    Y: Distribution<T>,
+{
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Polygon<T, N> {
+        Polygon {
+            vertices: [(); N].map(|_| Point {
+                x: self.x.sample(rng),
+                y: self.y.sample(rng),
+            }),
+        }
+    }
+}
 
 pub fn run() {
     let mut rng = thread_rng();
-    let dist = Bivariate::new(Uniform::new_inclusive(0, 10), Uniform::new_inclusive(0, 10));
-    let t = dist.sample(&mut rng);
-    println!("{:?}", t);
+    let dist = Bivariate {
+        x: Uniform::new(0, 2),
+        y: Uniform::new(2, 4),
+    };
+    let shape: Polygon<i32, 5> = dist.sample(&mut rng);
+    println!("{:?}", shape);
 }
