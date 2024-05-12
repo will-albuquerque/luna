eb159b987a0ce9c760794e8aad8f113ad8030412
Will Albuquerque <w.j.albuquerque@outlook.com>
2024-05-12 12:45:22 +0100

    Load image

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
-use luna::run;
+use std::path::PathBuf;
 
-fn main() {
-    run();
+use anyhow::Result;
+use clap::Parser;
+use image::io::Reader;
+
+#[derive(Parser)]
+struct Cli {
+    image: PathBuf,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    let image = Reader::open(cli.image)?.decode()?;
+    println!("{} {}", image.width(), image.height());
+
+    Ok(())
 }
